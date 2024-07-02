99a0be191f726f5a3eec0df6ee6b5d16911f242e
Daniel <daniel.j.crawford@outlook.com>
2024-07-02 12:31:11 -0400

    Removed uneeded property Players from game context

--- NumberGuessGame/GameContext.cs
+++ NumberGuessGame/GameContext.cs
@@ -6,7 +6,6 @@
         public const int MaxWinningNumber = 10;
 
         public int WinningNumber { get; set; }
-        public HashSet<string> Players { get; set; } = new HashSet<string>();
         public HashSet<string> Winners { get; set; } = new HashSet<string>();
 
         private Random random = new Random();
@@ -19,14 +18,11 @@
         public void Reset()
         {
             Winners.Clear();
-            Players.Clear();
             WinningNumber = random.Next(MinWinningNumber, MaxWinningNumber + 1);
         }
 
         public void Guess(string playerName, int guess)
         {
-            Players.Add(playerName);
-
             if (guess == WinningNumber)
             {
                 Winners.Add(playerName);
